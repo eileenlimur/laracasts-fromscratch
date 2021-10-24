20468bd8400e887bd0606de4e551e6b4bcee9a2f
Eileen Li <eileen@humancontact.com>
2021-10-23 23:15:56 -0400

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<script src="app.js"></script>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Eu in dolor non eu commodo do aliqua sunt ad tempor sint aute sit ea. Velit reprehenderit pariatur occaecat cupidatat minim ad commodo do proident. Consequat mollit id dolor enim ipsum officia occaecat cupidatat anim sunt ex dolor sit. Laboris velit ipsum ea irure voluptate sit cillum enim minim ad. Aliqua qui mollit nostrud nulla tempor ut fugiat cupidatat. Adipisicing occaecat in esse elit laboris eu. Ullamco Lorem non ad laboris anim ut id minim laborum quis duis elit Lorem.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Eu in dolor non eu commodo do aliqua sunt ad tempor sint aute sit ea. Velit reprehenderit pariatur occaecat cupidatat minim ad commodo do proident. Consequat mollit id dolor enim ipsum officia occaecat cupidatat anim sunt ex dolor sit. Laboris velit ipsum ea irure voluptate sit cillum enim minim ad. Aliqua qui mollit nostrud nulla tempor ut fugiat cupidatat. Adipisicing occaecat in esse elit laboris eu. Ullamco Lorem non ad laboris anim ut id minim laborum quis duis elit Lorem.</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-third-post">My Third Post</a></h1>
+        <p>Eu in dolor non eu commodo do aliqua sunt ad tempor sint aute sit ea. Velit reprehenderit pariatur occaecat cupidatat minim ad commodo do proident. Consequat mollit id dolor enim ipsum officia occaecat cupidatat anim sunt ex dolor sit. Laboris velit ipsum ea irure voluptate sit cillum enim minim ad. Aliqua qui mollit nostrud nulla tempor ut fugiat cupidatat. Adipisicing occaecat in esse elit laboris eu. Ullamco Lorem non ad laboris anim ut id minim laborum quis duis elit Lorem.</p>
+    </article>
+</body>
